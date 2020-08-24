2e99a907ba767515ba3d8fd5d92465116609ae35
Skye <ningsk0115@hotmail.com>
2020-08-24 10:12:23 +0800

    update

--- src/core/attr/index.js
+++ src/core/attr/index.js
@@ -4,9 +4,15 @@
  * @Author: 宁四凯
  * @Date: 2020-08-15 13:20:29
  * @LastEditors: 宁四凯
- * @LastEditTime: 2020-08-19 13:31:52
+ * @LastEditTime: 2020-08-24 10:11:42
  */
 export { default as AttrBillboard } from './AttrBillboard';
+export { default as AttrEllipsoid} from './AttrEllipsoid';
 export { default as AttrLabel } from './AttrLabel';
+export { default as AttrModel } from './AttrModel';
+export { default as AttrPoint } from './AttrPoint';
 export { default as AttrPolyline } from './AttrPolyline'
 export { default as AttrPolygon } from './AttrPolygon';
+export { default as AttrPolylineVolume } from './AttrPolylineVolume';
+export { default as AttrRectangle } from './AttrRectangle';
+export { default as AttrWall } from './AttrWall';
